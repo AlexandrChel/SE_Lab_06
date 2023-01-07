a306532911a088320da71a6c41ba794f0d714058
Alexandr Bychkov <dfcz179@yandex.ru>
2023-01-07 14:10:06 +0500

    Final edits

--- mycomplex.h
+++ mycomplex.h
@@ -14,33 +14,32 @@ using namespace std;
 \brief Класс комплексного числа
 */
 
-class Complex{
+class Complex
+{
 /**
 \brief Действительная чать
 */
-double Re;
+    double Re;
 /**
 \brief Минимая чать
 */
-double Im;
+    double Im;
 
-public:
-Complex(double aRe = 0, double aIm = 0);
-Complex(const Complex &);
-@@ -34,7 +51,7 @@ Complex &operator = (const Complex&);
-Complex &operator = (const double&);
-
-Complex &operator += (const Complex&);
-Complex &operator += (const double&);
-Complex &operator += (const double&);
-
-Complex &operator -= (const Complex&);
-Complex &operator -= (const double&);
-Complex &operator *= (const Complex&);
-Complex &operator *= (const double&);
-Complex &operator /= (const  double&);
-void Set(double aRe, double aIm);
-operator double();
-double abs();
-};
+    public:
+    Complex(double aRe = 0, double aIm = 0);
+    Complex(const Complex &);
+    Complex &operator = (const Complex&);
+    Complex &operator = (const double&);
+    Complex &operator += (const Complex&);
+    Complex &operator += (const double&);
+    Complex &operator -= (const Complex&);
+    Complex &operator -= (const double&);
+    Complex &operator *= (const Complex&);
+    Complex &operator *= (const double&);
+    Complex &operator /= (const Complex&);
+    Complex &operator /= (const double&);
+    void Set(double aRe, double aIm);
+    operator double();
+    double abs();
+}
 #endif
